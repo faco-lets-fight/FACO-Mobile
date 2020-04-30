883ec011ae79a4fee34ef23b4cc46dfb55b85dd5
Stephen Ojo <ojostephendev@gmail.com>
2020-04-30 10:16:34 +0100

    updated user attributes

--- app/src/main/java/com/facomobile/data/database/UserEntry.kt
+++ app/src/main/java/com/facomobile/data/database/UserEntry.kt
@@ -4,5 +4,5 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "user")
-data class UserEntry(@PrimaryKey val id: String = "", var fullName: String,
-                     var phoneNumber: String)
+data class UserEntry(@PrimaryKey val uid: String = "", var firstName: String,
+                     var lastName: String?, var otherName: String?, var phoneNumber: String?)
