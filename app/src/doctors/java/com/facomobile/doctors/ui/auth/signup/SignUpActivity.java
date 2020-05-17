259f0986866d73e1cf81ea548a6df3eff860a1d9
Stephen Ojo <ojostephendev@gmail.com>
2020-05-17 20:24:39 +0100

    File picker improved, sign up optimized

--- app/src/doctors/java/com/facomobile/doctors/ui/auth/signup/SignUpActivity.java
+++ app/src/doctors/java/com/facomobile/doctors/ui/auth/signup/SignUpActivity.java
@@ -1,29 +1,44 @@
 package com.facomobile.doctors.ui.auth.signup;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import com.bumptech.glide.Glide;
 import com.facomobile.R;
 import com.facomobile.utilities.InputUtils;
+import com.facomobile.views.CustomFilePicker;
+import com.github.chrisbanes.photoview.PhotoView;
 import com.google.android.material.snackbar.Snackbar;
 import com.hbb20.CountryCodePicker;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private static final int PHONE_VERIFICATION_REQUEST_CODE = 1010;
+    private static final int PICKFILE_RESULT_CODE = 1002;
     private InputUtils inputUtils;
     private View mRoot;
+    EditText etFirstName, etLastName, etOtherNames, etEmail, etPassword, etConfirmPassword,
+            etPhoneNumber, etDoctorType, etLicenseNumber;
+    private CustomFilePicker cfpDegreePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +47,17 @@ public class SignUpActivity extends AppCompatActivity {
 
         mRoot = findViewById(R.id.root);
 
-        EditText etFullname = findViewById(R.id.et_first_name),
-                etPassword = findViewById(R.id.et_password),
-                etPhoneNumber = findViewById(R.id.et_phone_number);
+        etFirstName = findViewById(R.id.et_first_name);
+        etLastName = findViewById(R.id.et_last_name);
+        etOtherNames = findViewById(R.id.et_other_names);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        etConfirmPassword = findViewById(R.id.et_confirm_password);
+        etPhoneNumber = findViewById(R.id.et_phone_number);
+        etDoctorType = findViewById(R.id.et_doctor_type);
+        etLicenseNumber = findViewById(R.id.et_license_number);
         ProgressBar pbWorking = findViewById(R.id.pb_working);
-        Button btnSubmit = findViewById(R.id.btn_submit);
+        Button btnContinue = findViewById(R.id.btn_continue);
 
         CountryCodePicker ccp = findViewById(R.id.ccp);
         ccp.registerCarrierNumberEditText(etPhoneNumber);
@@ -46,7 +67,7 @@ public class SignUpActivity extends AppCompatActivity {
             if (im != null) im.showSoftInput(etPhoneNumber, InputMethodManager.SHOW_IMPLICIT);
         }, 100));
 
-        btnSubmit.setOnClickListener(view -> {
+        btnContinue.setOnClickListener(view -> {
             if (isFormValid()) {
                 inputUtils.disableInput();
                 final String phoneNumber = ccp.getFullNumberWithPlus();
@@ -54,7 +75,28 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
-        inputUtils = InputUtils.init(pbWorking, ccp, etFullname, etPhoneNumber, etPassword, btnSubmit);
+        setupDegreePhotoPicker();
+
+        inputUtils = InputUtils.init(etFirstName, etLastName, etOtherNames, etEmail, etPassword,
+                etConfirmPassword, etPhoneNumber, etDoctorType, etLicenseNumber, pbWorking, ccp,
+                btnContinue, cfpDegreePhoto);
+    }
+
+    private void setupDegreePhotoPicker() {
+        cfpDegreePhoto = findViewById(R.id.cfp_degree_photo);
+        cfpDegreePhoto.setPickAction(() -> {
+            Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+            chooseFile.setType("image/*");
+            chooseFile = Intent.createChooser(chooseFile, "Select degree photo");
+            startActivityForResult(chooseFile, PICKFILE_RESULT_CODE);
+        });
+        cfpDegreePhoto.setPreviewAction(() -> {
+            DegreePhotoFragment degreePhotoFragment = new DegreePhotoFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("uid", cfpDegreePhoto.getDegreePhotoUri().toString());
+            degreePhotoFragment.setArguments(bundle);
+            degreePhotoFragment.show(getSupportFragmentManager(), "dialog");
+        });
     }
 
     @Override
@@ -85,6 +127,13 @@ public class SignUpActivity extends AppCompatActivity {
 
             }
             inputUtils.enableInput();
+        } else if (requestCode == PICKFILE_RESULT_CODE) {
+            if (resultCode == -1) {
+                Uri fileUri = data.getData();
+                if (fileUri != null) {
+                    cfpDegreePhoto.setDegreePhotoUri(fileUri);
+                }
+            }
         }
     }
 
@@ -123,5 +172,40 @@ public class SignUpActivity extends AppCompatActivity {
         }
 
         return result;
+    }
+
+    public static class DegreePhotoFragment extends DialogFragment {
+        PhotoView photoView;
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.degree_photo_dialog, container, false);
+            photoView = view.findViewById(R.id.img_profile_pic);
+            return view;
+        }
+
+        @Override
+        public void onStart() {
+            Bundle bundle = getArguments();
+            if (bundle != null) {
+                String filePath = bundle.getString("uid");
+                if (!TextUtils.isEmpty(filePath)) {
+                    Glide.with(getContext())
+                            .load(Uri.parse(filePath))
+                            .placeholder(R.drawable.default_profile_pic)
+                            .into(photoView);
+                }
+            }
+            super.onStart();
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            Dialog dialog = super.onCreateDialog(savedInstanceState);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            return dialog;
+        }
     }
 }
--- app/src/main/java/com/facomobile/views/CustomFilePicker.java
+++ app/src/main/java/com/facomobile/views/CustomFilePicker.java
@@ -1,6 +1,7 @@
 package com.facomobile.views;
 
 import android.content.Context;
+import android.net.Uri;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,13 +13,10 @@ import androidx.annotation.Nullable;
 
 import com.facomobile.R;
 
-import java.io.File;
-
 public class CustomFilePicker extends LinearLayout {
 
-    private File mPickedFile;
+    private Uri mUri;
 
-    private LinearLayout mRoot;
     private TextView mTvPicker;
     private Button mBtnPreview;
 
@@ -28,13 +26,8 @@ public class CustomFilePicker extends LinearLayout {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (inflater != null) {
             View mFilePicker = inflater.inflate(R.layout.custom_file_picker_view, this, true);
-            mRoot = mFilePicker.findViewById(R.id.ll_file_picker_root);
             mTvPicker = mFilePicker.findViewById(R.id.tv_file_picker);
             mBtnPreview = mFilePicker.findViewById(R.id.btn_file_picker_preview);
-
-            mTvPicker.setOnClickListener(view -> launchFilePicker());
-
-//            mRoot.setOnClickListener(view -> Toast.makeText(context, "Hey heyyyyy", Toast.LENGTH_SHORT).show());
         }
     }
 
@@ -42,9 +35,24 @@ public class CustomFilePicker extends LinearLayout {
         this(context, null);
     }
 
-    private void launchFilePicker() {
-
+    public void setPickAction(Runnable callback) {
+        mTvPicker.setOnClickListener(view -> callback.run());
     }
 
+    public void setPreviewAction(Runnable callback) {
+        mBtnPreview.setOnClickListener(view -> callback.run());
+    }
 
+    public Uri getDegreePhotoUri() {
+        return mUri;
+    }
+
+    public void setDegreePhotoUri(Uri uri) {
+        mUri = uri;
+        mBtnPreview.setEnabled(mUri != null);
+    }
+
+    public boolean isPhotoPicked() {
+        return mUri != null;
+    }
 }
